cb6282942aae0d362931cfac5a29863c8a4f3a78
VladimirBalun <vladimirbalun@yandex.ru>
2021-07-12 21:35:16 +0300

    feat: add Easy/ValidParentheses

--- Easy/ValidParentheses.cpp
+++ Easy/ValidParentheses.cpp
@@ -1,4 +1,37 @@
-//
-// Created by v.balun on 05.06.2021.
-//
+#include <stack>
+#include <iostream>
 
+bool is_valid_parentheses(const std::string& text)
+{
+    std::stack<char> opened_parentheses;
+    for (const auto& symbol : text) {
+        if ((symbol == '(') || (symbol == '[') || (symbol == '{')) {
+            opened_parentheses.push(symbol);
+        } else {
+            const char last_symbol = opened_parentheses.top();
+            opened_parentheses.pop();
+            if ((symbol == ')') && (last_symbol == '(')) {
+                continue;
+            }
+            if ((symbol == ']') && (last_symbol == '[')) {
+                continue;
+            }
+            if ((symbol == '}') && (last_symbol == '{')) {
+                continue;
+            }
+            return false;
+        }
+    }
+    return opened_parentheses.empty();
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "Result: " << is_valid_parentheses("()") << std::endl;
+    std::cout << "Result: " << is_valid_parentheses("()[]{}") << std::endl;
+    std::cout << "Result: " << is_valid_parentheses("(]") << std::endl;
+    std::cout << "Result: " << is_valid_parentheses("([)]") << std::endl;
+    std::cout << "Result: " << is_valid_parentheses("{[]}") << std::endl;
+
+    return EXIT_SUCCESS;
+}
