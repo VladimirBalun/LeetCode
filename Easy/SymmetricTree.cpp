d0d9aa78b92fcd2bd917b2fcf4fff444a66f1c1e
VladimirBalun <vladimirbalun@yandex.ru>
2021-08-19 22:39:46 +0300

    refactor: improve SymmetricTree

--- Easy/SymmetricTree.cpp
+++ Easy/SymmetricTree.cpp
@@ -1,50 +1,48 @@
-#include <iostream>
+#include "../Helpers.hpp"
 
-struct TreeNode
+class Solution
 {
-    int data = 0;
-    std::unique_ptr<TreeNode> left = nullptr;
-    std::unique_ptr<TreeNode> right = nullptr;
-};
-
-bool is_symmetric_tree(const std::unique_ptr<TreeNode>& root_tree_node)
-{
-    if (!root_tree_node) {
-        return false;
-    }
-
-    std::function<bool(const std::unique_ptr<TreeNode>&, const std::unique_ptr<TreeNode>&)> is_symmetric_impl;
-    is_symmetric_impl = [&is_symmetric_impl](const std::unique_ptr<TreeNode>& lhs, const std::unique_ptr<TreeNode>& rhs) {
-        if ((!lhs) && (!rhs)) {
-            return true;
-        } else if ((!lhs) || (!rhs)) {
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        if (!root) {
             return false;
-        } else {
-            return (lhs->data == rhs->data) &&
-                (is_symmetric_impl(lhs->left, rhs->right)) &&
-                (is_symmetric_impl(lhs->right, rhs->left));
         }
-    };
 
-    return is_symmetric_impl(root_tree_node, root_tree_node);
-}
+        std::function<bool(const TreeNode*, const TreeNode*)> is_symmetric_impl;
+        is_symmetric_impl = [&is_symmetric_impl](const TreeNode* lhs, const TreeNode* rhs) {
+            if ((!lhs) && (!rhs)) {
+                return true;
+            } else if ((!lhs) || (!rhs)) {
+                return false;
+            } else {
+                return (lhs->val == rhs->val) &&
+                       (is_symmetric_impl(lhs->left, rhs->right)) &&
+                       (is_symmetric_impl(lhs->right, rhs->left));
+            }
+        };
+
+        return is_symmetric_impl(root, root);
+    }
+};
 
 int main(int argc, char** argv)
 {
+    Solution solution;
     {
-        std::unique_ptr<TreeNode> root(new TreeNode{10, nullptr, nullptr});
-        std::cout << "Is symmetric: " << is_symmetric_tree(root) << std::endl;
+        TreeNode root{10, nullptr, nullptr};
+        ASSERT_EQUALS(solution.isSymmetric(&root), true);
     }
     {
-        std::unique_ptr<TreeNode> left(new TreeNode{10, nullptr, nullptr});
-        std::unique_ptr<TreeNode> right(new TreeNode{10, nullptr, nullptr});
-        std::unique_ptr<TreeNode> root(new TreeNode{20, std::move(left), std::move(right)});
-        std::cout << "Is symmetric: " << is_symmetric_tree(root) << std::endl;
+        TreeNode left{10, nullptr, nullptr};
+        TreeNode right{10, nullptr, nullptr};
+        TreeNode root{20, &left, &right};
+        ASSERT_EQUALS(solution.isSymmetric(&root), true);
     }
     {
-        std::unique_ptr<TreeNode> right(new TreeNode{10, nullptr, nullptr});
-        std::unique_ptr<TreeNode> root(new TreeNode{20, nullptr, std::move(right)});
-        std::cout << "Is symmetric: " << is_symmetric_tree(root) << std::endl;
+        TreeNode right{10, nullptr, nullptr};
+        TreeNode root{20, nullptr, &right};
+        ASSERT_EQUALS(solution.isSymmetric(&root), false);
     }
     return EXIT_SUCCESS;
 }
