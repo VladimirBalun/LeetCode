4439e935474086fa9264607e1a6f49cabe954d07
VladimirBalun <vladimirbalun@yandex.ru>
2021-08-19 23:14:21 +0300

    refactor: improve 2Sum

--- Easy/2Sum.cpp
+++ Easy/2Sum.cpp
@@ -1,53 +1,41 @@
 #include <vector>
-#include <iostream>
 #include <unordered_map>
 
-std::vector<size_t> find_number_sum(const std::vector<int>& numbers, int target_number)
-{
-    std::vector<size_t> result{};
-    std::unordered_map<int, size_t> table{};
-    for (size_t i = 0u; i < numbers.size(); ++i) {
-        const auto it = table.find(target_number - numbers[i]);
-        if (it != end(table)) {
-            result.push_back(i);
-            result.push_back(it->second);
-            return result;
-        } else {
-            table.emplace(numbers[i], i);
-        }
-    }
+#include "../Helpers.hpp"
 
-    return result;
-}
+class Solution
+{
+public:
+    std::vector<int> twoSum(const std::vector<int>& nums, int target)
+    {
+        std::unordered_map<int, size_t> table{};
+        for (size_t i = 0u; i < nums.size(); ++i) {
+            const auto it = table.find(target - nums[i]);
+            if (it != end(table)) {
+                return { static_cast<int>(it->second), static_cast<int>(i) };
+            } else {
+                table.emplace(nums[i], i);
+            }
+        }
+
+        return {};
+    }
+};
 
 int main(int argc, char** argv)
 {
+    Solution solution;
     {
         const std::vector<int> data{ 2, 7, 11, 15 };
-        const std::vector<size_t> result = find_number_sum(data, 9);
-        if (result.empty()) {
-            std::cout << "[ ]" << std::endl;
-        } else {
-            std::cout << "[ " << result.at(0u) << ", " << result.at(1u) << " ]" << std::endl;
-        }
+        ASSERT_EQUALS(solution.twoSum(data, 9), (std::vector<int>{ 0, 1 }));
     }
     {
         const std::vector<int> data{ 3, 2, 4 };
-        const std::vector<size_t> result = find_number_sum(data, 6);
-        if (result.empty()) {
-            std::cout << "[ ]" << std::endl;
-        } else {
-            std::cout << "[ " << result.at(0u) << ", " << result.at(1u) << " ]" << std::endl;
-        }
+        ASSERT_EQUALS(solution.twoSum(data, 6), (std::vector<int>{ 1, 2 }));
     }
     {
         const std::vector<int> data{ 3, 3 };
-        const std::vector<size_t> result = find_number_sum(data, 6);
-        if (result.empty()) {
-            std::cout << "[ ]" << std::endl;
-        } else {
-            std::cout << "[ " << result.at(0u) << ", " << result.at(1u) << " ]" << std::endl;
-        }
+        ASSERT_EQUALS(solution.twoSum(data, 6), (std::vector<int>{ 0, 1 }));
     }
     return EXIT_SUCCESS;
 }
