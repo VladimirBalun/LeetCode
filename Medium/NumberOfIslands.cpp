93bb6a4724c854fb4d16f3c9b8b564b8a30de917
VladimirBalun <vladimirbalun@yandex.ru>
2021-08-20 10:48:28 +0300

    feat: add NumberOfIslands

--- Medium/NumberOfIslands.cpp
+++ Medium/NumberOfIslands.cpp
@@ -1,4 +1,93 @@
-//
-// Created by Владимир Балун on 18.08.2021.
-//
+#include "../Helpers.hpp"
 
+class Solution
+{
+public:
+    int numIslands(std::vector<std::vector<char>> grid)
+    {
+        std::function<void(std::vector<std::vector<char>>&, int, int)> bfs_impl;
+        bfs_impl = [&bfs_impl](std::vector<std::vector<char>>& grid, int i, int j) {
+            if ((i < 0) || (i >= grid.size()) || (j < 0) || (j >= grid[i].size()) || (grid[i][j] == '0')) {
+                return;
+            }
+
+            grid[i][j] = '0';
+            bfs_impl(grid, i + 1, j);
+            bfs_impl(grid, i - 1, j);
+            bfs_impl(grid, i, j + 1);
+            bfs_impl(grid, i, j - 1);
+        };
+
+        int count_islands = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    ++count_islands;
+                    bfs_impl(grid, i, j);
+                }
+            }
+        }
+
+        return count_islands;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Solution solution;
+    {
+        const std::vector<std::vector<char>> grid{
+                { '0', '0' },
+                { '0', '0' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 0);
+    }
+    {
+        const std::vector<std::vector<char>> grid{
+                { '1', '0' },
+                { '0', '1' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 2);
+    }
+    {
+        const std::vector<std::vector<char>> grid{
+                { '1', '0' },
+                { '0', '1' },
+                { '0', '1' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 2);
+    }
+    {
+        const std::vector<std::vector<char>> grid{
+                { '1', '0', '0' },
+                { '0', '1', '1' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 2);
+    }
+    {
+        const std::vector<std::vector<char>> grid{
+                { '1', '1' },
+                { '1', '1' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 1);
+    }
+    {
+        const std::vector<std::vector<char>> grid{
+            { '1', '1', '1', '1', '0' },
+            { '1', '1', '0', '1', '0' },
+            { '1', '1', '0', '0', '0' },
+            { '0', '0', '0', '0', '0' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 1);
+    }
+    {
+        const std::vector<std::vector<char>> grid{
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        ASSERT_EQUALS(solution.numIslands(grid), 3);
+    }
+    return EXIT_SUCCESS;
+}
