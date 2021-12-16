4b843edabdb3534dd00f181ea0d99ea690b7adae
VladimirBalun <vladimirbalun@yandex.ru>
2021-12-16 07:20:44 +0300

    refactor: improve problem solvings

--- Algorithms/Easy/BalancedBinaryTree.cpp
+++ Algorithms/Easy/BalancedBinaryTree.cpp
@@ -1,5 +1,21 @@
 #include "../Helpers.hpp"
 
+// Given a binary tree, determine if it is height-balanced.
+// For this problem, a height-balanced binary tree is defined as:
+// a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+
+// Example 3:
+// Input: root = []
+// Output: true
+
 class Solution
 {
 public:
