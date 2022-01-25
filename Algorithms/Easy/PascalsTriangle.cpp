bdcc979adda90803e257141038a0d386260b372e
VladimirBalun <vladimirbalun@yandex.ru>
2022-01-25 09:54:37 +0300

    feat: add new problem solvings

--- Algorithms/Easy/PascalsTriangle.cpp
+++ Algorithms/Easy/PascalsTriangle.cpp
@@ -14,21 +14,15 @@
 class Solution
 {
 public:
-    std::vector<vector<int>> generate(int numRows)
+    std::vector<std::vector<int>> generate(int numRows)
     {
-        if (numRows <= 0) {
-            return {};
-        }
-
-        std::vector<vector<int>> result;
-        result.emplace_back(vector<int>{ 1 });
-        for (int i = 0u; i < numRows - 1; ++i) {
-            const vector<int>& prev_row = result.back();
-            std::vector<int> current_row(prev_row.size() + 1u);
-            for (size_t j = 0u; j < current_row.size(); ++j) {
-                const int left = (j > 0) ? (prev_row[j - 1]) : (0);
-                const int right = (j < current_row.size() - 1u) ? (prev_row[j]) : (0);
-                current_row[j] = left + right;
+        std::vector<std::vector<int>> result;
+        result.push_back(std::vector<int>(1, 1));
+        for (size_t i = 2u; i <= numRows; ++i) {
+            const std::vector<int>& prev_row = result[i - 1];
+            std::vector<int> current_row(i, 1);
+            for (size_t i = 1u; i < current_row.size() - 1u) {
+                current_row[i] = prev_row[i - 1] + prev_row[i];
             }
             result.push_back(std::move(current_row));
         }
