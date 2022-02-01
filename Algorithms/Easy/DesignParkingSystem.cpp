f03490c505c106737710c2ab024a7ec9453e77e5
VladimirBalun <vladimirbalun@yandex.ru>
2022-02-01 11:27:10 +0300

    feat: DesignParkingSystem

---
+++ Algorithms/Easy/DesignParkingSystem.cpp
@@ -0,0 +1,39 @@
+#include "../Helpers.hpp"
+
+// Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+// Implement the ParkingSystem class:
+// ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+// bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+
+// Example 1:
+// Input
+// ["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+// [[1, 1, 0], [1], [2], [3], [1]]
+// Output
+// [null, true, true, false, false]
+
+// Explanation
+// ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+// parkingSystem.addCar(1); // return true because there is 1 available slot for a big car
+// parkingSystem.addCar(2); // return true because there is 1 available slot for a medium car
+// parkingSystem.addCar(3); // return false because there is no available slot for a small car
+// parkingSystem.addCar(1); // return false because there is no available slot for a big car. It is already occupied.
+
+class ParkingSystem
+{
+public:
+    ParkingSystem(int big, int medium, int small)
+        : m_places{ big, medium, small } {}
+    
+    bool addCar(int car_type)
+    {
+        return (--m_places[car_type - 1] >= 0);
+    }
+private:
+    std::array<int, 3u> m_places;
+};
+
+int main(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
