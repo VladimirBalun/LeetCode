440964371e91b24e451a5c308d85357c780ea59d
VladimirBalun <vladimirbalun@yandex.ru>
2022-03-04 09:12:47 +0300

    feat: add AddDigits

---
+++ Algorithms/Easy/AddDigits.cpp
@@ -0,0 +1,39 @@
+#include "../Helpers.hpp"
+
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+// Example 1:
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+
+// Example 2:
+// Input: num = 0
+// Output: 0
+
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        while (num > 9) {
+            int temp = num;
+            int result = 0;
+            while (temp != 0) {
+                result += temp % 10;
+                temp /= 10;
+            }
+            num = result;
+        }
+
+        return num;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
