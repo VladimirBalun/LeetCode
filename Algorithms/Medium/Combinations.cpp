90551f6cd2dc49b1bbfd55f8f443d4aba5e97b59
v.balun <v.balun@corp.mail.ru>
2022-03-24 09:34:43 +0300

    feat: add Combinations

---
+++ Algorithms/Medium/Combinations.cpp
@@ -0,0 +1,36 @@
+#include "../Helpers.hpp"
+
+// Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+// You may return the answer in any order.
+
+// Example 1:
+// Input: n = 4, k = 2
+// Output:
+// [
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+// ]
+
+// Example 2:
+// Input: n = 1, k = 1
+// Output: [[1]]
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> combine(int n, int k)
+    {
+        for (int i = 1; i <= n; ++i) {
+
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
