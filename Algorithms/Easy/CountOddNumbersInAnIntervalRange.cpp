644bb290172c8db4ae2d05c6147a8dcf482e32a9
VladimirBalun <vladimirbalun@yandex.ru>
2022-04-11 08:58:20 +0300

    refactor: improve CountOddNumbersInAnIntervalRange

--- Algorithms/Easy/CountOddNumbersInAnIntervalRange.cpp
+++ Algorithms/Easy/CountOddNumbersInAnIntervalRange.cpp
@@ -1,5 +1,26 @@
 #include "../Helpers.hpp"
 
+// Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+// Example 1:
+// Input: low = 3, high = 7
+// Output: 3
+// Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+// Example 2:
+// Input: low = 8, high = 10
+// Output: 1
+// Explanation: The odd numbers between 8 and 10 are [9].
+
+class Solution
+{
+public:
+    int countOdds(int low, int high)
+    {
+        return (high - low) / 2 + !(low % 2 == 0 && high % 2 == 0);
+    }
+};
+
 int main(int argc, char** argv)
 {
     return EXIT_SUCCESS;
