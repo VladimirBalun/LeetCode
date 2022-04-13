c300274e7cb0b10c30dfd9c8530a14dea1200bf4
VladimirBalun <vladimirbalun@yandex.ru>
2022-04-13 10:08:32 +0300

    feat: add SignOfTheProductOfAnArray

---
+++ Algorithms/Easy/SignOfTheProductOfAnArray.cpp
@@ -0,0 +1,46 @@
+#include "../Helpers.hpp"
+
+// There is a function signFunc(x) that returns:
+
+// 1 if x is positive.
+// -1 if x is negative.
+// 0 if x is equal to 0.
+// You are given an integer array nums. Let product be the product of all values in the array nums.
+// Return signFunc(product).
+
+// Example 1:
+// Input: nums = [-1,-2,-3,-4,3,2,1]
+// Output: 1
+// Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+
+// Example 2:
+// Input: nums = [1,5,0,2,-3]
+// Output: 0
+// Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+
+// Example 3:
+// Input: nums = [-1,1,-1,1,-1]
+// Output: -1
+// Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+
+class Solution
+{
+public:
+    int arraySign(const std::vector<int>& nums)
+    {
+        int sign = 1;
+        for (size_t i = 0u; i < nums.size(); ++i) {
+            if (nums[i] == 0)
+                return 0;
+            else if (nums[i] < 0)
+                sign = -sign;
+        }
+
+        return sign;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
