13f9dd63289e360600da26961b0d0ba4808210ba
VladimirBalun <vladimirbalun@yandex.ru>
2022-06-21 09:59:11 +0300

    feat: add SwapNodesInPairs

---
+++ Algorithms/Medium/SwapNodesInPairs.cpp
@@ -0,0 +1,49 @@
+#include "../Helpers.hpp"
+
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+// Example 1:
+// Input: head = [1,2,3,4]
+// Output: [2,1,4,3]
+
+// Example 2:
+// Input: head = []
+// Output: []
+
+// Example 3:
+// Input: head = [1]
+// Output: [1]
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        if (!head || !head->next)
+            return head;
+        
+        ListNode* current = head;
+        ListNode* previous = nullptr;
+        ListNode* dummy = head->next;
+        while (current && current->next) {
+            ListNode* temp = current;
+            current = current->next;
+            
+            temp->next = current->next;
+            current->next = temp;
+            
+            if (previous) {
+                previous->next = current;
+            }
+            
+            previous = current->next;
+            current = temp->next;
+        }
+        
+        return dummy;
+    }
+};
+
+int main(int argc, char** argv)
+{
+	return EXIT_SUCCESS;
+}
