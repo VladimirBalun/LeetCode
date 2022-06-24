904d205e447faf3e23279a9dfd3a8388c5fca8d1
VladimirBalun <vladimirbalun@yandex.ru>
2022-06-24 09:42:52 +0300

    feat: add CoinChange

---
+++ Algorithms/Medium/CoinChange.cpp
@@ -0,0 +1,47 @@
+#include "../Helpers.hpp"
+
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You may assume that you have an infinite number of each kind of coin.
+
+// Example 1:
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+
+// Example 2:
+// Input: coins = [2], amount = 3
+// Output: -1
+
+// Example 3:
+// Input: coins = [1], amount = 0
+// Output: 0
+
+class Solution
+{
+public:
+    int coinChange(vector<int>& coins, int amount)
+    {
+        std::vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        
+        std::sort(begin(coins), end(coins));
+        for (int i = 1; i < dp.size(); ++i) {
+            for (const int coin : coins) {
+                const int diff = i - coin;
+                if (diff < 0) {
+                    break;
+                }
+                
+                dp[i] = std::min(dp[i], 1 + dp[diff]); 
+            }
+        }
+
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
+    }
+};
+
+int main(int argc, char** argv)
+{
+	return EXIT_SUCCESS;
+}
