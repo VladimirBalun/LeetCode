4f7ce9b11089ba226e39a7790bbee86465ba9260
VladimirBalun <vladimirbalun@yandex.ru>
2022-08-09 09:45:28 +0300

    feat: add PowerOfFour

---
+++ Algorithms/Easy/PowerOfFour.cpp
@@ -0,0 +1,30 @@
+#include "../Helpers.hpp"
+
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+// Example 1:
+// Input: n = 16
+// Output: true
+
+// Example 2:
+// Input: n = 5
+// Output: false
+
+// Example 3:
+// Input: n = 1
+// Output: true
+
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;    
+    }
+};
+
+int main(int argc, char** argv)
+{
+	return EXIT_SUCCESS;
+}
