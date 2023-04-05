313bacb1dc5b3539099ccea1ed4fce46f060c2f6
VladimirBalun <vladimirbalun@yandex.ru>
2023-04-05 09:44:08 +0300

    feat: add BackspaceStringCompare

---
+++ Algorithms/Easy/BackspaceStringCompare.cpp
@@ -0,0 +1,94 @@
+#include "../Helpers.hpp"
+
+// Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+// Note that after backspacing an empty text, the text will continue empty.
+
+// Example 1:
+// Input: s = "ab#c", t = "ad#c"
+// Output: true
+// Explanation: Both s and t become "ac".
+
+// Example 2:
+// Input: s = "ab##", t = "c#d#"
+// Output: true
+// Explanation: Both s and t become "".
+
+// Example 3:
+// Input: s = "a#c", t = "b"
+// Output: false
+// Explanation: s becomes "c" while t becomes "b".
+
+class Solution
+{
+public:
+    bool backspaceCompare(std::string s, std::string t)
+    {
+        int left = s.size() - 1;
+        int right = t.size() - 1;
+
+        int left_offset = 0;
+        int right_offset = 0;
+
+        while (left >= 0 && right >= 0) {
+            while (left >= 0) {
+                if (s[left] == '#') {
+                    ++left_offset;
+                    --left;
+                } else if (left_offset > 0) {
+                    --left_offset;
+                    --left;
+                } else {
+                    break;
+                }
+            }
+
+            while (right >= 0) {
+                if (t[right] == '#') {
+                    ++right_offset;
+                    --right;
+                } else if (right_offset > 0) {
+                    --right_offset;
+                    --right;
+                } else {
+                    break;
+                }
+            }
+
+            std::cout << left << " - " << right << std::endl;
+            if (left >= 0 && right >= 0 && s[left] != t[right])
+                return false;
+            
+            //if ((left_offset >= 0) != (right_offset >= 0))
+            //   return false;
+    
+            --left;
+            --right;
+        }
+
+        while (left >= 0) {
+            if (s[left] == '#') {
+                ++left_offset;
+                --left;
+            } else if (left_offset > 0) {
+                --left_offset;
+                --left;
+            } else {
+                break;
+            }
+        }
+
+        while (right >= 0) {
+            if (t[right] == '#') {
+                ++right_offset;
+                --right;
+            } else if (right_offset > 0) {
+                --right_offset;
+                --right;
+            } else {
+                break;
+            }
+        }
+
+        return left == right;
+    }
+};
