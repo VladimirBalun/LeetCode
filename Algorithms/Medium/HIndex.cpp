9dc1e2e3e345ca23086a89fe3eb0dd405c88eb0b
VladimirBalun <vladimirbalun@yandex.ru>
2023-11-28 09:44:11 +0300

    feat: add HIndex.cpp

---
+++ Algorithms/Medium/HIndex.cpp
@@ -0,0 +1,36 @@
+#include "../Helpers.hpp"
+
+// Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+// According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+// Example 1:
+// Input: citations = [3,0,6,1,5]
+// Output: 3
+// Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+// Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+// Example 2:
+// Input: citations = [1,3,1]
+// Output: 1
+
+class Solution
+{
+public:
+    int hIndex(std::vector<int>& citations)
+    {
+        std::vector<int> frequencies(1000 + 1);
+        for (const int citation: citations)
+            ++frequencies[citation];
+
+        int count = 0;
+        for (int citation = frequencies.size() - 1; citation >= 0; --citation) {
+            const int frequency = frequencies[citation];
+            count += frequency;
+
+            if (count >= citation)
+                return citation;
+        }
+        
+        return 0;
+    }
+};
